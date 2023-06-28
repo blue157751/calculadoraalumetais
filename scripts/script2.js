2d5d20e5c70c03ead23a2cd5f6a583d905e11d6d
blue157751 <86863784+blue157751@users.noreply.github.com>
2023-06-27 21:51:48 -0300

    Adicionando uma função nova
    Adicionei uma função nova que implementa um novo parágrafo e dá o resultado em dinheiro do cálculo da sucata e do refile, fazendo com que apareça a mensagem de quantos KG o cliente poderá levar (o que já aparecia por padrão) e quanto ele recebe em dinheiro, caso ele queira vender a sucata e o refile.

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,30 +2,35 @@
 const formularioDois = document.querySelector('#formularioMaoDeObra');
 
 formularioDois.addEventListener('submit' , function(eventoDois) {
-eventoDois.preventDefault();
+    eventoDois.preventDefault();
 
-const inputSucata = eventoDois.target.querySelector('#sucata');
-const inputRefile = eventoDois.target.querySelector('#refile');
+    const inputSucata = eventoDois.target.querySelector('#sucata');
+    const inputRefile = eventoDois.target.querySelector('#refile');
 
-const sucata = Number(inputSucata.value.replace(',', '.'));
-const refile = Number(inputRefile.value.replace(',', '.'));
+    const sucata = Number(inputSucata.value.replace(',', '.'));
+    const refile = Number(inputRefile.value.replace(',', '.'));
 
-if (inputSucata.value && isNaN(sucata)) {
-    setResultadoDois('Peso da Sucata inválido' , false);
-    return;
-}
+    if (inputSucata.value && isNaN(sucata)) {
+        setResultadoDois('Peso da Sucata inválido' , false);
+        return;
+    }
 
-if (inputRefile.value && isNaN(refile)) {
-    setResultadoDois('Peso do Refile inválido', false);
-    return;
-}
+    if (inputRefile.value && isNaN(refile)) {
+        setResultadoDois('Peso do Refile inválido', false);
+        return;
+    }
 
-const calcDois = getCalcDois(sucata, refile);
+    const calcDois = getCalcDois(sucata, refile);
+    const msg = `O cliente pode levar ${calcDois} Kg na mão de obra.`
 
-const msg = `O cliente pode levar ${calcDois} Kg na mão de obra.`
+    setResultadoDois(msg, true);
 
-setResultadoDois(msg, true);
-
+    // Adicionando o novo parágrafo com o valor em dinheiro
+    const valorMaoDeObra = getValorMaoDeObra(sucata, refile);
+    const resultadoDois = document.querySelector('#resultadoDois');
+    const p2 = criaP();
+    p2.innerHTML = `Se o cliente for vender, ele receberá R$: ${valorMaoDeObra}`;
+    resultadoDois.appendChild(p2);
 });
 
 function getCalcDois(sucata, refile) {
@@ -37,6 +42,18 @@ function getCalcDois(sucata, refile) {
         calcDois += refile - (refile * (35 / 100));
     }
     return calcDois.toFixed(2);
+}
+
+// Função para calcular o valor em dinheiro da mão de obra
+function getValorMaoDeObra(sucata, refile) {
+    let valorMaoDeObra = 0;
+    if (sucata) {
+        valorMaoDeObra += (sucata * 9.50);
+    }
+    if (refile) {
+        valorMaoDeObra += (refile * 8.50);
+    }
+    return valorMaoDeObra.toFixed(2);
 }
 
 function criaP () {
